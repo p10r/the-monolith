90af622eda0a84f30d5521db3b4ab82ac7a18877
Philipp Mayer <phlppmayer@gmail.com>
2023-12-17 12:15:37 +0100

    rm old

--- domain/artist_registry.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "context"
-
-type ArtistRegistry interface {
-	Add(ctx context.Context, artist NewArtist) (Artist, error)
-	FindAll(ctx context.Context) (Artists, error)
-}
--- domain/db_event.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type DbEvent struct {
-	Msg string `json:"msg"`
-}
-
-func (e DbEvent) EventName() string {
-	return "DbEvent"
-}
