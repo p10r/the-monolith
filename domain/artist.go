8f8ee85565cfb800f077afdfebb0fca91b2e1ba1
Philipp Mayer <phlppmayer@gmail.com>
2024-01-29 22:25:26 +0100

    move EventIDs to event.go

--- domain/artist.go
+++ domain/artist.go
@@ -2,8 +2,6 @@ package domain
 
 import (
 	"slices"
-	"strconv"
-	"strings"
 )
 
 type Artist struct {
@@ -59,25 +57,3 @@ func (a Artists) RASlugs() []RASlug {
 type UserID int64
 
 type UserIDs []UserID
-type EventID int64
-
-type EventIDs []EventID
-
-// NewEventID should be replaced with this:
-// https://stackoverflow.com/questions/51923863/how-to-construct-json-so-i-can-receive-int64-and-string-using-golang
-func NewEventID(id string) (EventID, error) {
-	i, err := strconv.ParseInt(strings.TrimSpace(id), 10, 64)
-	if err != nil {
-		return EventID(0), err
-	}
-	return EventID(i), nil
-}
-
-func (eventId EventIDs) Contains(id EventID) bool {
-	var ints []int64
-	for _, eventID := range eventId {
-		ints = append(ints, int64(eventID))
-	}
-
-	return slices.Contains(ints, int64(id))
-}
--- domain/event.go
+++ domain/event.go
@@ -1,5 +1,11 @@
 package domain
 
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
 type Event struct {
 	Id         string
 	Title      string
@@ -10,3 +16,26 @@ type Event struct {
 }
 
 type Events []Event
+
+type EventID int64
+
+type EventIDs []EventID
+
+// NewEventID should be replaced with this:
+// https://stackoverflow.com/questions/51923863/how-to-construct-json-so-i-can-receive-int64-and-string-using-golang
+func NewEventID(id string) (EventID, error) {
+	i, err := strconv.ParseInt(strings.TrimSpace(id), 10, 64)
+	if err != nil {
+		return EventID(0), err
+	}
+	return EventID(i), nil
+}
+
+func (eventId EventIDs) Contains(id EventID) bool {
+	var ints []int64
+	for _, eventID := range eventId {
+		ints = append(ints, int64(eventID))
+	}
+
+	return slices.Contains(ints, int64(id))
+}
